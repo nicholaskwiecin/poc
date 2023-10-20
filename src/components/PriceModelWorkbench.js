f8e438e08c8d67d24fefc406cb079cf3fd2eabd0
Nicholas Kwiecinski <nicholas.kwiecinski@avanade.com>
2023-10-20 11:25:30 -0400

    Build base cost formula tree and support for feedstock nodes

--- src/components/PriceModelWorkbench.js
+++ src/components/PriceModelWorkbench.js
@@ -5,7 +5,7 @@ import ReactFlow, {
   useNodesState,
   useEdgesState,
   Controls,
-  Background 
+  Background
 } from 'reactflow';
 import './PriceModelWorkbench.scss';
 import 'reactflow/dist/style.css';
@@ -19,68 +19,138 @@ const options = { hideAttribution: true };
 const snapGrid = [20, 20];
 
 const initialNodes = [
-  { id: '1_category', position: { x: 300, y: 0 }, data: { 
-    label: 'Total Materials',
-    className:'nodrag',
-    type: 'category' 
-  } },
-  { id: '1_formula', position: { x: 300, y: 0 }, data: { 
-    label: 'Raw Materials',
-    type: 'formula_sum' 
-  } },
-  { id: '2_formula', position: { x: 300, y: 0 }, data: { 
-    label: 'Packaging',
-    type: 'formula_sum' 
-  } },
-  { id: '1_category', position: { x: 300, y: 0 }, data: { 
-    label: 'Conversion and Fees',
-    className:'nodrag',
-    type: 'category' 
-  } },
-  { id: '3_formula', position: { x: 300, y: 0 }, data: { 
-    label: 'Manufacturing',
-    type: 'formula_sum' 
-  } },
-  { id: '4_formula', position: { x: 300, y: 0 }, data: { 
-    label: 'Labor',
-    type: 'formula_sum' 
-  } },
-  { id: '5_formula', position: { x: 300, y: 0 }, data: { 
-    label: 'Fees',
-    type: 'formula_sum' 
-  } },
-  { id: '1_category', position: { x: 300, y: 0 }, data: { 
-    label: 'Logistics',
-    className:'nodrag',
-    type: 'category' 
-  } },
-  { id: '2_formula', position: { x: 300, y: 0 }, data: { 
-    label: 'Transportation',
-    type: 'formula_sum' 
-  } },
-  { id: '2_formula', position: { x: 300, y: 0 }, data: { 
-    label: 'Warehousing',
-    type: 'formula_sum' 
-  } },
-
-
-
-
-  { id: '2_input_materials', position: { x: 0, y: 100 }, data: { label: 'Feedstock 1', type: 'input_materials', value: 50.4 } },
-  { id: '3_input_materials', position: { x: 200, y: 100 }, data: { label: 'Feedstock 2', type: 'input_materials', value: 16.6 } },
-  { id: '4_input_materials', position: { x: 400, y: 100 }, data: { label: 'Feedstock 3', type: 'input_materials', value: 5 } },
-  { id: '5_input_materials', position: { x: 600, y: 100 }, data: { label: 'Feedstock 4', type: 'input_materials', value: 16.6 } },
+  {
+    id: '1_header',
+    position: { x: 0, y: -50 },
+    type: 'input',
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Price Model Header / Total Invoice Price',
+      type: 'header'
+    }
+  },
+  {
+    id: '1_category',
+    position: { x: -400, y: 100 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Total Materials',
+      type: 'category'
+    }
+  },
+  {
+    id: '1_formula',
+    position: { x: -500, y: 200 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Raw Materials',
+      type: 'formula_sum'
+    }
+  },
+  {
+    id: '2_formula',
+    position: { x: -300, y: 200 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Packaging',
+      type: 'formula_sum'
+    }
+  },
+  {
+    id: '2_category',
+    position: { x: 0, y: 100 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Conversion and Fees',
+      type: 'category'
+    }
+  },
+  {
+    id: '3_formula',
+    position: { x: -150, y: 200 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Manufacturing',
+      type: 'formula_sum'
+    }
+  },
+  {
+    id: '4_formula',
+    position: { x: 0, y: 200 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Labor',
+      type: 'formula_sum'
+    }
+  },
+  {
+    id: '5_formula',
+    position: { x: 150, y: 200 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Fees',
+      type: 'formula_sum'
+    }
+  },
+  {
+    id: '3_category',
+    position: { x: 400, y: 100 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Logistics',
+      type: 'category'
+    }
+  },
+  {
+    id: '6_formula',
+    position: { x: 500, y: 200 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Transportation',
+      type: 'formula_sum'
+    }
+  },
+  {
+    id: '7_formula',
+    position: { x: 300, y: 200 },
+    draggable: false,
+    deletable: false,
+    data: {
+      label: 'Warehousing',
+      type: 'formula_sum'
+    }
+  },
 ];
 
 const initialEdges = [
-  { id: 'e1-2', source: '1_formula', target: '2_input_materials' },
-  { id: 'e1-3', source: '1_formula', target: '3_input_materials' },
-  { id: 'e1-4', source: '1_formula', target: '4_input_materials' },
-  { id: 'e1-5', source: '1_formula', target: '5_input_materials' }
+  { id: 'h1-1', source: '1_header', target: '1_category' },
+  { id: 'h1-2', source: '1_header', target: '2_category' },
+  { id: 'h1-3', source: '1_header', target: '3_category' },
+  { id: 'c1-1', source: '1_category', target: '1_formula' },
+  { id: 'c1-2', source: '1_category', target: '2_formula' },
+  { id: 'c2-3', source: '2_category', target: '3_formula' },
+  { id: 'c2-4', source: '2_category', target: '4_formula' },
+  { id: 'c2-5', source: '2_category', target: '5_formula' },
+  { id: 'c3-6', source: '3_category', target: '6_formula' },
+  { id: 'c3-7', source: '3_category', target: '7_formula' }
 ];
 
-let id = 0;
-const getId = () => `costElement_${id++}`;
+let id = 1;
+const getId = () => id++;
+const getVendorID = () => Math.random()*100000000;
+const getMaterialID = () => Math.random()*100000000;
+const getUsage = () => Math.random()*100;
+
 
 const PriceModelWorkbench = () => {
 
@@ -100,7 +170,6 @@ const PriceModelWorkbench = () => {
   }, []);
 
   const onSelectionChange = useCallback((elements) => {
- 
 
     // Return if no nodes selected
     if (!elements.nodes.length) {
@@ -109,18 +178,25 @@ const PriceModelWorkbench = () => {
 
     // TODO: Handle multiple selections, right now we populate based on the first element
     let searchElement = elements.nodes[0]
+    let formulaNode;
 
-    // TODO:  Travese up multiple levels in the tree, right now this will only work if you select a child node of the formula node
-    // Travese the edges to get a formula_sum type node
-    let formulaNode = nodes.find((node)=>node.id===edges.find((edge) => edge.target === searchElement.id && edge.source.includes('formula')).source)
+    // If you selected a formula node directly, use that
+    if (searchElement.id.includes('formula')) {
+      formulaNode = searchElement;
+    }
+    else {
+      // TODO:  Travese up multiple levels in the tree, right now this will only work if you select a child node of the formula node
+      // Travese the edges to get a formula_sum type node
+      formulaNode = nodes.find((node) => node.id === edges.find((edge) => edge.target === searchElement.id && edge.source.includes('formula')).source)
+    }
+
     let inputNodes = [];
-    
-    for (let edge of edges.filter((edge)=>edge.source===formulaNode.id)) {
-      inputNodes.push(nodes.find((node)=>edge.target===node.id));
+    for (let edge of edges.filter((edge) => edge.source === formulaNode.id)) {
+      inputNodes.push(nodes.find((node) => edge.target === node.id));
     };
 
-    console.log({formulaNode, inputNodes});
-    setSelectedFormula({formulaNode, inputNodes});
+    console.log({ formulaNode, inputNodes });
+    setSelectedFormula({ formulaNode, inputNodes });
   }, []);
 
   const onNodeContextMenu = useCallback(
@@ -131,6 +207,8 @@ const PriceModelWorkbench = () => {
       // Calculate position of the context menu. We want to make sure it
       // doesn't get positioned off-screen.
       const pane = reactFlowWrapper.current.getBoundingClientRect();
+
+      // TODO: Fix the menu positioning
       setMenu({
         id: node.id,
         top: event.clientY < pane.height - 200 && event.clientY,
@@ -153,7 +231,8 @@ const PriceModelWorkbench = () => {
       const elementName = event.dataTransfer.getData('application/reactflow');
 
       // check if the dropped element is valid
-      if (typeof elementName === 'undefined' || !elementName) {
+      // TODO: Implement other element types, right now we only support Feedstock elements
+      if (typeof elementName === 'undefined' || !elementName || elementName !== 'Feedstock') {
         return;
       }
 
@@ -161,10 +240,20 @@ const PriceModelWorkbench = () => {
         x: event.clientX - reactFlowBounds.left,
         y: event.clientY - reactFlowBounds.top,
       });
+      const inputId = getId();
       const newNode = {
-        id: getId(),
+        id: `${inputId}_input_${elementName}`,
         position,
-        data: { label: elementName },
+        data: { 
+          label: `${elementName} ${inputId}`,
+          type: `input_${elementName}`,
+          value: 0,
+          vendorId: getVendorID(),
+          usage: getUsage(),
+          materialID: getMaterialID(),
+          unit: 'KG',
+          usageUnit: 'Percent',
+         },
       };
 
       setNodes((nds) => nds.concat(newNode));
@@ -195,18 +284,18 @@ const PriceModelWorkbench = () => {
             proOptions={options}
             fitView
           ><Controls />
-           <Background />
-          {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
+            <Background />
+            {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
           </ReactFlow>
         </div>
         <div className="left-panel">
           <CostElementLibrary />
         </div>
         <div className="right-panel">
-          <CostingPanel selectedFormula={selectedFormula}/>
+          <CostingPanel selectedFormula={selectedFormula} />
         </div>
         <div className="bottom-panel">
-          <FormulaBar selectedFormula={selectedFormula}/>
+          <FormulaBar selectedFormula={selectedFormula} />
         </div>
       </ReactFlowProvider>
     </div>

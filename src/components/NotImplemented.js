f18f4856368f093526a23a047778479c4562e5e8
Seth Lakstins <seth.lakstins@avanade.com>
2023-10-30 14:57:56 -0400

    back button added to NotImplemented page

--- src/components/NotImplemented.js
+++ src/components/NotImplemented.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import './NotImplemented.scss';
 
 function NotImplemented() {
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    }
     return (
         <div>
+            <button onClick={goBack}><FontAwesomeIcon className="fa-lg" icon={faArrowLeft}></FontAwesomeIcon></button>
             <p>This functionality has not been implemented as part of this Proof of Concept</p>
         </div>
     );
